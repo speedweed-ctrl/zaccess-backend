697da2cf0141c6d008b2be392d017e00fdd0b901
skander nafti <skander.nafti@ieee.org>
2025-05-26 12:32:42 +0100

    added statit root

--- backend/settings.py
+++ backend/settings.py
@@ -137,6 +137,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
