845de151ae91a5f733ea7e5f3bddb8dbc6c5483e
skander nafti <skander.nafti@ieee.org>
2025-05-26 12:47:28 +0100

    config for static files

--- backend/settings.py
+++ backend/settings.py
@@ -136,7 +136,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
